ac10420320d29ca028252387a867aff52eb5fc8b
Sai Vivek <saivivekbunny69@gmail.com>
2025-07-07 16:15:09 +0530

    Two pointer approach to check if array is Palindrome or not

---
+++ two-pointer-js-problems/palindrome-check.js
@@ -0,0 +1,15 @@
+let arr=['r','a','c','e','c','a','r'];
+let isPalindrome=true;
+
+let left=0;
+let right=arr.length-1;
+while(left<=right){
+    if(arr[left]!=arr[right]){
+        isPalindrome=false;
+        break;
+       
+    }
+    left=left+1;
+    right=right-1;
+}
+console.log(isPalindrome)
