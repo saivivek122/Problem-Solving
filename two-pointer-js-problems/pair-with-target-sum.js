7635490707828bca011f52644ab50eafe8d11d2f
Sai Vivek <saivivekbunny69@gmail.com>
2025-07-07 16:09:27 +0530

    code for pair with sum using two pointer approach

---
+++ two-pointer-js-problems/pair-with-target-sum.js
@@ -0,0 +1,20 @@
+let arr=[1, 2, 4, 7, 11, 15];
+let target=15;
+let left=0;
+let right=arr.length-1;
+let sum=0;
+while(left <=right){
+    sum=arr[left]+arr[right];
+     if(sum==target){
+        console.log(left,right)
+        break;
+    }
+    else if(sum<target){
+        left=left+1;
+    }
+    else {
+        right=right-1;
+    }
+  
+   
+}   
